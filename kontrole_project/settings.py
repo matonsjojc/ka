907dd7f98cf7325c5c97a2c8baa9fee999758ac3
matonsjojc <matevz.podlipnik@gmail.com>
2016-06-07 00:25:46 +0200

    do ch.5

--- kontrole_project/settings.py
+++ kontrole_project/settings.py
@@ -17,6 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -120,5 +122,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
