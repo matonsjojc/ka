2bd07cbc83e5ebb62465ac210b5be618ef82d734
matonsjojc <matevz.podlipnik@gmail.com>
2016-06-07 22:59:07 +0200

    prvi modeli

--- ka/models.py
+++ ka/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Pacient(models.Model):
+    user = models.OneToOneField(User)
+    aparat = models.OneToOneField('Aparat')
+    maska = models.OneToOneField('Maska')
+    ponudnik = models.ForeignKey('Ponudnik')
+    #dodaj še id, odgovore
+    def __str__(self):
+        return self.user.username
+
+class Aparat(models.Model):
+    proizvajalec = models.CharField(max_length=120)
+    vrsta_terapije = models.CharField(max_length=120)
+    ponudnik = models.ForeignKey('Ponudnik')
+    #dodaj še: model, nastavitev,
+    def __str__(self):
+        return self.proizvajalec
+
+class Maska(models.Model):
+    tip = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.tip
+
+class Ponudnik(models.Model):
+    ime = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.ime
+
+#dodaj modela vprasanje, odgovor
